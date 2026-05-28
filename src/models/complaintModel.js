99f87914d8ee5d5c41eeefa3c66b333aea256537
kaue0000 <kaueund003@gmail.com>
2026-05-27 21:00:36 -0300

    Validação do horário de envio da foto implementado

--- src/models/complaintModel.js
+++ src/models/complaintModel.js
@@ -4,6 +4,24 @@ const complaintsCollection = db.collection('complaints')
 
 const ComplaintModel = {
     async create(data){
+        // Valida se os metadados EXIF foram enviados
+        if (!data.exif) throw new Error('Metadados da foto são obrigatórios')
+
+        const { dateTaken } = data.exif
+
+        if (!dateTaken) throw new Error('Data de captura da foto é obrigatória')
+
+        // Verifica se a foto foi tirada hoje
+        const photoDate = new Date(dateTaken)
+        const today = new Date()
+
+        const isSameDay = photoDate.getDate() === today.getDate() && photoDate.getMonth() === today.getMonth() && photoDate.getFullYear === today.getFullYear
+
+        if(!isSameDay) throw new Error('A foto deve ser tirada no momento da denúncia')
+
+        const diffInMinutes = (today - photoDate) / 1000 / 60
+        if(diffInMinutes > 60) throw new Error('A foto deve ser tirada no momento da denúncia')
+        
         const doc = await complaintsCollection.add({
             title: data.title,
             description: data.description,
